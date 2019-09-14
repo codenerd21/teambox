a9c29c72144c5469839ffab1aa0b796383e2b2ce
Braxton Toomer <braxton.toomer@gmail.com>
2019-09-14 15:03:26 -0700

    adds template app, adds home and about views, and routes to urls.py

--- teambox/main_app/views.py
+++ teambox/main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+  return HttpResponse('<h1>Home Page - Returning HTTP Response</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About the Team Box</h1>')
+
+#Code below from Tasty
+
+
+# def home(request):
+#     return render(
+#         request,
+#         'main/home.html',
+#     )
