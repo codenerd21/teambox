dfb8db74fcf5b85a52d554bdc7b8d45b95cd1f0b
Braxton Toomer <braxton.toomer@gmail.com>
2019-09-14 16:59:34 -0700

    adds template for teams/index, adds routes for teams in urls, adds fake class to views to act as model

--- teambox/main_app/views.py
+++ teambox/main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Team():  
+  def __init__(self, name, grade, gender):
+    self.name = name
+    self.grade = grade
+    self.gender = gender
+
+teams = [
+  Team('Mid City', '6th', 'Boys'),
+  Team('Willowside-1', '7th', 'Boys'),
+  Team('CYO-1', '6th', 'Boys'),
+  Team('CYO-2', '8th', 'Girls'),
+  Team('Analy', '9th', 'Girls'),
+  Team('Willowside-2', '8th', 'Girls'),
+]
+
 def home(request):
   return HttpResponse('<h1>Home Page - Returning HTTP Response</h1>')
 
@@ -8,3 +23,5 @@ def about(request):
   return render(request, 'about.html')
 
 
+def teams_index(request):
+  return render(request, 'teams/index.html', {'teams': teams})
