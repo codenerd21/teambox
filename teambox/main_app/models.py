222ccc3702d62ebc9e7d7a306cf0361c55297910
Braxton Toomer <braxton.toomer@gmail.com>
2019-09-18 07:52:16 -0700

    updates player model

--- teambox/main_app/models.py
+++ teambox/main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+POSITIONS = (
+  ('PG', 'Point Guard'),
+  ('SG', 'Shooting Guard'),
+  ('SF', 'Small Forward'),
+  ('PF', 'Power Forward'),
+  ('C', 'Center'),
+)
+
 class Team(models.Model):
   name = models.CharField(max_length=30)
   grade = models.IntegerField()
@@ -29,8 +37,12 @@ class Stat(models.Model):
 class Player(models.Model):
   first_name = models.CharField(max_length=20)
   number = models.IntegerField()
-  position = models.CharField(max_length=2)
+  position = models.CharField(
+    max_length=2,
+    choices=POSITIONS,
+    default=POSITIONS[0][0]
+  )
 
   def __str__(self):
-    return f'{self.first_name} ({self.id})'
+    return f'{self.get_position_display()}'
   
