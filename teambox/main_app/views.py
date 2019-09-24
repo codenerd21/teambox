12e68586f95362424f866fded1fccafe1dd9941d
Braxton Toomer <braxton.toomer@gmail.com>
2019-09-23 19:03:46 -0700

    Final edits and submission

--- teambox/main_app/views.py
+++ teambox/main_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from itertools import chain
 import boto3
 import uuid
